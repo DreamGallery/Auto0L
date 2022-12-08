f8c70b2f0dfaa821465e140d813a0067779842d0
DreamGallery <49429799+DreamGallery@users.noreply.github.com>
2022-12-09 02:55:45 +0800

    Update Auto0L.sh

--- Auto0L.sh
+++ Auto0L.sh
@@ -3,30 +3,35 @@ trap "pkill -P $$" SIGINT
 
 export PATH=$PATH:~/bin && echo export PATH=\$PATH:~/bin >> ~/.bashrc
 curl -sL https://raw.githubusercontent.com/OLSF/libra/main/ol/util/install.sh | bash
-wget https://github.com/DreamGallery/libra/releases/download/v5.2.0/tower -O ~/bin/tower && chmod +x ~/bin/tower
+wget -q https://github.com/DreamGallery/libra/releases/download/v5.2.0/tower -O ~/bin/tower && chmod +x ~/bin/tower
 
 ONBOARDMNEMONIC="Input your 0L mnemonic here"
 onboard keygen > keygen.log
 Address=$(cat keygen.log | sed -n "3p")
 AutheneticationKey=$(cat keygen.log | sed -n "7p")
+MNEMONIC=$(cat keygen.log | sed -n "11p" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+echo "Address: ${Address}"
+echo "MNEMONIC: ${MNEMONIC}"
 echo "AutheneticationKey: ${AutheneticationKey}"
-MNEMONIC=$(cat keygen.log | sed -n "11p")
 
-./gettemplate.sh ${MNEMONIC}
+./gettemplate.sh "${MNEMONIC}"
+PID=$!; wait ${PID}
+
 
 sed -i 's/upstream_nodes = \[.*\]/upstream_nodes = \["http:\/\/52.15.236.78:8080", "http:\/\/73.181.115.53:8080", "http:\/\/fullnode.letsmove.fun"\]/g' ~/.0L/0L.toml
 sed -i "$(($(cat ~/.0L/0L.toml | grep -n "tx_configs.miner_txs_cost" | awk -F ":" '{print $1}')+1))c max_gas_unit_for_tx = 20000" ~/.0L/0L.toml
 
 
-while [[ $(curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"get_account","params":["'$Address'"],"id":1}' http://0lexplorer.io/api/proxy/node | jq -r '.result') == "null" ]]
+while [[ $(curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"get_account","params":["'${Address}'"],"id":1}' http://0lexplorer.io/api/proxy/node | jq -r '.result') == "null" ]]
 do
-    ./onboard.sh ${ONBOARDMNEMONIC} ${AutheneticationKey}
+    ./onboard.sh "${ONBOARDMNEMONIC}" "${AutheneticationKey}"
+    PID=$!; wait ${PID}
 done
 
-while [[ $(curl -s https://0l.interblockcha.in:444/epochs/proofs/$Address) == \[\] ]]
+while [[ $(curl -s https://0l.interblockcha.in:444/epochs/proofs/${Address}) == \[\] ]]
 do
-    ./submitgenesis.sh ${MNEMONIC}
+    ./submitgenesis.sh "${MNEMONIC}"
+    PID=$!; wait ${PID}
 done
 
-wget https://github.com/OLSF/libra/releases/download/v5.2.0/tower -O ~/bin/tower && chmod +x ~/bin/tower
-./AutoLocalTower.sh
+wget -q https://github.com/OLSF/libra/releases/download/v5.2.0/tower -O ~/bin/tower && chmod +x ~/bin/tower
